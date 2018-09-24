9ebdb551dcf5c45ebd8c784c47b3c3945e5068af
kml1326 <kmlraj131996@gmail.com>
2018-09-24 22:50:48 +0530

    adding day 19 folder

--- Day 18-20180922T143845Z-001/Day 18/assets/snakeGame.js
+++ Day 18-20180922T143845Z-001/Day 18/assets/snakeGame.js
@@ -1,9 +1,9 @@
-var wrapper = document.querySelector('.wrapper');
+var board = document.querySelector('.board');
+var playBtn = document.querySelector('.play-btn')
 
+for(i=0 ; i<40; i++) {
+	for(j=0; j<30; j++) {
+		board.innerHTML = board.innerHTML + `<div class="cell" data-row=${i}  data-col=${j}></div>`;
 
-for(i=0 ; i<25; i++) {
-	for(j=0; j<25; j++) {
-		wrapper.innerHTML = wrapper.innerHTML + `<div class="cell" data-row=${i}  data-col=${j}></div>`;
-		// console.log(i, j);
 	}
 }
